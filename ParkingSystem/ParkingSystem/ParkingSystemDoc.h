437ae722cc473eb8e73d4afffa688e50ec3f494a
questan <questan@tencent.com>
2019-09-30 15:58:19 +0800

    init

---
+++ ParkingSystem/ParkingSystem/ParkingSystemDoc.h
@@ -0,0 +1,48 @@
+
+// ParkingSystemDoc.h : CParkingSystemDoc 类的接口
+//
+
+
+#pragma once
+
+
+class CParkingSystemDoc : public CDocument
+{
+protected: // 仅从序列化创建
+	CParkingSystemDoc();
+	DECLARE_DYNCREATE(CParkingSystemDoc)
+
+// 特性
+public:
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CParkingSystemDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
